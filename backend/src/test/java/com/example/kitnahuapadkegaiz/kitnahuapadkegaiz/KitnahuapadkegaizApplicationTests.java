c51befd9f4062b008cda179389475ff93db3279d
Unchanted <omkkdwivedi@gmail.com>
2024-10-16 20:05:29 +0530

    backend

---
+++ backend/src/test/java/com/example/kitnahuapadkegaiz/kitnahuapadkegaiz/KitnahuapadkegaizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kitnahuapadkegaiz.kitnahuapadkegaiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KitnahuapadkegaizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
