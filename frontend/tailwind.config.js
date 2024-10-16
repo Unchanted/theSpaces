56de426305486aab478f77695728bd0512a53f03
Unchanted <omkkdwivedi@gmail.com>
2024-10-16 22:56:50 +0530

    removed flowbite react

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", flowbite.content()],
+  content: [],
   theme: {
     extend: {},
   },
-  plugins: [flowbite.plugin()],
+  plugins: [],
 };
