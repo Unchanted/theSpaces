e2f4ec7dddcc024cba81d3a173d9a9116de9b38d
Omik Acharya <omiktrash@gmail.com>
2024-10-22 01:35:57 +0530

    pooksBaba added in gupshup

--- frontend/src/pages/Gupshup.jsx
+++ frontend/src/pages/Gupshup.jsx
@@ -1,8 +1,16 @@
+import CreateSpaceIcon from "../assets/CreateSpace.svg";
+
 function Gupshup() {
   return (
-    <div className="p-6 text-xl text-black/80 font-bold italic bg-background">
-      Coming soon ...
+    
+    <div className="flex flex-col items-center">
+    <img src={CreateSpaceIcon} alt="Create Space Icon" className="mb-4" />
+    <div className="flex items-center">
+      <div className="p-6 text-xl text-black/80 font-bold italic bg-background">
+        Coming soon ...
+      </div>
     </div>
+  </div>
   );
 }
 
