9c130b7f43febb9df0d3beebede5d99144728578
Om Lanke <92863779+OmLanke@users.noreply.github.com>
2024-11-03 23:32:03 +0530

    auto reload cache

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), VitePWA({
+    registerType: 'autoUpdate',
     includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
     manifest: {
       name: 'theSpaces',
